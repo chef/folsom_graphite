08ef7b47a35af9d15cd2ca2b4f53bfd8d99923ee
James Casey <james@opscode.com>
2014-01-02 10:35:30 +0000

    Refactor extract_values
    Remove the accumulator and foldl and replace with simple list
    concatation since we're building an iolist and the nesting is ignored.
    
    Add tests for the simple 1-line cases (counter/gauge) of metric
    generation

--- src/folsom_graphite_worker.erl
+++ src/folsom_graphite_worker.erl
@@ -94,39 +94,49 @@ code_change(_OldVsn, State, _Extra) ->
 -spec publish_to_graphite(#state{}) -> ok.
 publish_to_graphite(#state{prefix = Prefix}) ->
     Timestamp = folsom_graphite_util:make_timestamp(),
-    {_, _, Lines} = lists:foldl(fun extract_value/2, {Prefix, Timestamp, []}, folsom_metrics:get_metrics_info()),
-    folsom_graphite_sender:send(Lines),
-    ok.
+    Lines = construct_all_lines(Prefix, Timestamp),
 
-%% folsom has added an extra field to the second tuple returned from
-%% folsom_metrics:get_all_metrics().  We drop it out here
-extract_value({Name, [{type, Type}, _Tags]}, {Prefix, Timestamp, Acc}) ->
-    extract_value({Name, [{type, Type}]}, {Prefix, Timestamp, Acc});
+    folsom_graphite_sender:send(Lines).
 
-extract_value({Name, [{type, histogram}]}, {Prefix, Timestamp, Acc}) ->
+%% construct the set of graphite lines for all metrics
+%% in folsom
+-spec construct_all_lines(Prefix :: string(),
+                          Timestamp :: string()) -> iolist().
+construct_all_lines(Prefix, Timestamp) ->
+    [extract_value(MetricInfo, Prefix, Timestamp) || MetricInfo <- folsom_metrics:get_metrics_info()].
+
+%% Construct the graphite lines for a single metric.  In the case of
+%% more complex metric types (historgram, meter) this could return
+%% multiple lines of output
+%%
+%% NOTE: folsom has added an extra field to the second tuple returned from
+%% folsom_metrics:get_all_metrics() containing tags.  We drop it out here
+%% so we can support folsom before or after this API change
+extract_value({Name, [{type, Type}, _Tags]}, Prefix, Timestamp) ->
+    extract_value({Name, [{type, Type}]}, Prefix, Timestamp);
+
+extract_value({Name, [{type, counter}]}, Prefix, Timestamp) ->
+    Value = folsom_metrics:get_metric_value(Name),
+    folsom_graphite_util:graphite_format(Prefix, Timestamp, {io_lib:format("~s.count", [Name]), Value});
+extract_value({Name, [{type, gauge}]}, Prefix, Timestamp) ->
+    Value = folsom_metrics:get_metric_value(Name),
+    folsom_graphite_util:graphite_format(Prefix, Timestamp, {io_lib:format("~s.value", [Name]), Value});
+extract_value({Name, [{type, histogram}]}, Prefix, Timestamp) ->
     Values = folsom_metrics:get_histogram_statistics(Name),
-    {_, _, Acc1} = extract_values(Name, Values, ?HISTOGRAM_FIELDS, {Prefix, Timestamp, Acc}),
+    HistogramFields = extract_values(Name, Values, ?HISTOGRAM_FIELDS, Prefix, Timestamp),
     Percentiles = proplists:get_value(percentile, Values),
-    extract_values(Name, Percentiles, ?PERCENTILE_FIELDS, {Prefix, Timestamp, Acc1});
-extract_value({Name, [{type, gauge}]}, {Prefix, Timestamp, Acc}) ->
-    Value = folsom_metrics:get_metric_value(Name),
-    {Prefix, Timestamp, [folsom_graphite_util:graphite_format(Prefix, Timestamp, {io_lib:format("~s.value", [Name]), Value})|Acc]};
-extract_value({Name, [{type, meter}]}, {Prefix, Timestamp, Acc}) ->
+    [HistogramFields | extract_values(Name, Percentiles, ?PERCENTILE_FIELDS, Prefix, Timestamp)];
+extract_value({Name, [{type, meter}]}, Prefix, Timestamp) ->
     Values = folsom_metrics:get_metric_value(Name),
-    extract_values(Name, Values, ?METER_FIELDS, {Prefix, Timestamp, Acc});
-extract_value({Name, [{type, counter}]}, {Prefix, Timestamp, Acc}) ->
-    Value = folsom_metrics:get_metric_value(Name),
-    {Prefix, Timestamp, [folsom_graphite_util:graphite_format(Prefix, Timestamp, {io_lib:format("~s.count", [Name]), Value})|Acc]};
-extract_value({_Name, [{type, _}]}, {Prefix, Timestamp, Acc}) ->
-    {Prefix, Timestamp, [Acc]}.
+    extract_values(Name, Values, ?METER_FIELDS, Prefix, Timestamp);
+extract_value({_Name, [{type, _}]}, _Prefix, _Timestamp) ->
+    [].
 
-extract_values(Name, Values, Fields, {Prefix, Timestamp, StartAcc}) ->
-    {Prefix, Timestamp, lists:foldl(fun({MetricName, PrettyName}, Acc) ->
-                                            V = proplists:get_value(MetricName, Values),
-                                            [folsom_graphite_util:graphite_format(Prefix, Timestamp, {io_lib:format("~s.~s",[Name, PrettyName]), V})| Acc]
-                                    end,
-                                    StartAcc,
-                                    Fields)}.
+extract_values(Name, Values, Fields, Prefix, Timestamp) ->
+    [ begin
+            V = proplists:get_value(MetricName, Values),
+            folsom_graphite_util:graphite_format(Prefix, Timestamp, {io_lib:format("~s.~s",[Name, PrettyName]), V})
+      end || {MetricName, PrettyName} <- Fields].
 
 -spec append_hostname(Prefix :: string()) -> string().
 append_hostname(undefined) ->
--- test/folsom_graphite_worker_tests.erl
+++ test/folsom_graphite_worker_tests.erl
@@ -63,6 +63,17 @@ prefix_test_() ->
 
 extract_value_test_() ->
     MockedModules = [],
+    Prefix = "prefix",
+    TS = "12345678",
+    ExpectedFun = fun(Key, Value) ->
+            list_to_binary(io_lib:format("~s.~s ~w ~s~n", [Prefix, Key, Value, TS]))
+    end,
+    TestLineFun = fun(Name, Value, Type, Expected) ->
+            folsom_metrics:notify(Name, Value, Type),
+            [M] = folsom_metrics:get_metric_info(Name),
+            Line = folsom_graphite_worker:extract_value(M, Prefix, TS),
+            ?assertEqual(Expected, list_to_binary(Line))
+    end,
     {foreach,
      fun() -> test_util:setup(MockedModules),
               application:start(folsom)
@@ -70,11 +81,16 @@ extract_value_test_() ->
      fun(_) -> application:stop(folsom),
                test_util:cleanup(MockedModules)
         end,
-    [
-        {"test with no subs",
-         fun() ->
-                    ok
-            end}
+     [
+        {"extract_value for a Counter",
+         fun() -> Expected = ExpectedFun("count1.count", 1),
+                  TestLineFun("count1", {inc, 1}, counter, Expected)
+                end
+        },
+        {"extract_value for a Gauge",
+         fun() -> Expected = ExpectedFun("gauge1.value", 1),
+                  TestLineFun("gauge1", 1, gauge, Expected)
+                end
+        }
     ]}.
-
 
