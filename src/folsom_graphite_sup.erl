b5eef87d194b03822d72629d0b0ccba62b5c6ba1
Oliver Ferrigni <oliver@opscode.com>
2014-10-27 15:14:00 -0500

    Correct fun heads for when application is started

--- src/folsom_graphite_sup.erl
+++ src/folsom_graphite_sup.erl
@@ -12,9 +12,11 @@
 %% Supervisor callbacks
 -export([init/1]).
 
-%-ifdef(TEST).
+
+-ifdef(TEST).
+-compile([export_all]).
 -include_lib("eunit/include/eunit.hrl").
-%-endif.
+-endif.
 
 %% Helper macro for declaring children of supervisor
 -define(SUP(I, Args), {I, {I, start_link, Args}, permanent, infinity, supervisor, [I]}).
@@ -45,10 +47,15 @@ get_env(Key, Default) ->
             Default
     end.
 
+%This is the case that application is not started
 maybe_start_sender_worker([]) ->
     lager:debug("No config found, not starting sender or worker"),
     [];
-maybe_start_sender_worker(_) ->
+%This is the case that application is started
+maybe_start_sender_worker([{included_applications, _} | []]) ->
+    lager:debug("No config found, not starting sender or worker"),
+    [];
+maybe_start_sender_worker(_Message) ->
     GraphiteHost = get_env(graphite_host, "localhost"),
     GraphitePort = get_env(graphite_port, 2003),
     Prefix = get_env(prefix, "folsom"),
